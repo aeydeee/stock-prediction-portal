b1a40ab99d6f97b6f178b3113232649314a349b9
Adrian Samuel <aeydeee@gmail.com>
2025-06-29 20:07:48 +0800

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,16 @@
 import {useState} from 'react'
+import './assets/css/style.css'
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
+            <Header/>
+            <Main/>
+            <Footer/>
 
         </>
     )
